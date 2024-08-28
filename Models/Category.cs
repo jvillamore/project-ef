9f20dafaf67a65ef4b7057ed93a42ad2e97f5603
Juan Villafuerte Mollinedo <villafuerte.j@outlook.com>
2024-08-28 11:09:41 -0400

    Creación de los métodos para leer y guardar nformación

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace project_ef.Models;
 
@@ -20,5 +21,6 @@ public class Category
 
     public DateTime UpdateAt { get; set; }
 
+    [JsonIgnore]
     public virtual ICollection<Task> Tasks { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using project_ef;
+using project_ef.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 // Consiguración para añadir la base de datos en memoria.
@@ -14,6 +15,20 @@ app.MapGet("/dbMemoryConexion", ([FromServices] TareasContext dbContext) =>
 {
     dbContext.Database.EnsureCreated();
     return Results.Ok($"Database created: {dbContext.Database.IsInMemory()}");
+});
+app.MapGet("/api/tareas", ([FromServices] TareasContext dbContext) =>
+{
+    return Results.Ok(dbContext.Tasks.Include(s => s.Category).Where(s => s.PrioridadTarea == project_ef.Models.Priority.High).ToList());
+});
+
+app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromBody] project_ef.Models.Task task) =>
+{
+    task.TaskId = Guid.NewGuid();
+    task.CreationAt = DateTime.Now;
+    //await dbContext.AddAsync(task);
+    await dbContext.Tasks.AddAsync(task);
+    await dbContext.SaveChangesAsync();
+    return Results.Ok();
 });
 
 app.Run();
